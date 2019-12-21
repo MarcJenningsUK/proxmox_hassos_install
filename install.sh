ed7a32643ed85708bdf20b935884379b323110e3
MarcJenningsUK <marc.jenning.s@gmail.com>
2019-12-21 13:42:49 +0000

    Update install.sh
    upped boot disk to 16gb

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ qm create $VMID -bios ovmf -name $(sed -e "s/\_//g" -e "s/.${RELEASE_EXT}//" <<<
 pvesm alloc $STORAGE $VMID $DISK0 128 1>&/dev/null
 qm importdisk $VMID ${FILE%".gz"} $STORAGE $IMPORT_OPT 1>&/dev/null
 qm set $VMID -bootdisk sata0 -efidisk0 ${DISK0_REF},size=128K \
-    -sata0 ${DISK1_REF},size=6G > /dev/null
+    -sata0 ${DISK1_REF},size=16G > /dev/null
 echo -e "\n\n\n" \
     "********************************\n" \
     "*    Completed Successfully    *\n" \
